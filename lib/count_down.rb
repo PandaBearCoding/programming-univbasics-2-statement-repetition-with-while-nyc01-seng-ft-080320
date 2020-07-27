13344fe94e8c46d80427ec5c17ace28982545f30
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-27 18:07:40 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,15 @@
 # Write your code here
+
+celebration_time = 1
+count = 10
+
+while (condition) do 
+  
+end 
+
+
+while count > celebration_time do 
+  break if count == celebration_time
+  puts "#{count}"
+  count = count - 1
+end 
